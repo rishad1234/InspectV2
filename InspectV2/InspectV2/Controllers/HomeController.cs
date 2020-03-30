eeb398935d426f426341985067a030bc30dd9940
Shahriar1212 <43474871+Shahriar1212@users.noreply.github.com>
2020-03-30 14:06:49 +0600

    Home page added
    home page added with name index2,

--- InspectV2/InspectV2/Controllers/HomeController.cs
+++ InspectV2/InspectV2/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace InspectV2.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index2()
         {
             return View();
         }
