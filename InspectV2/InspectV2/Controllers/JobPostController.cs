bef863cef85b4dade65ca786e9f424ff43a6d610
Shahriar1212 <43474871+Shahriar1212@users.noreply.github.com>
2020-04-16 20:37:11 +0600

    Job Post page added but have a problem in sponsored image size

---
+++ InspectV2/InspectV2/Controllers/JobPostController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InspectV2.Controllers
+{
+    public class JobPostController : Controller
+    {
+        // GET: JobPost
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
