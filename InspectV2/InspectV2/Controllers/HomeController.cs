a62bd5a444032c3eb03c9c7ae8ff0dad771b4608
Shahriar1212 <43474871+Shahriar1212@users.noreply.github.com>
2020-04-18 16:23:17 +0600

    news is now comming from database, but category not fixed
    news details still left to be added

--- InspectV2/InspectV2/Controllers/HomeController.cs
+++ InspectV2/InspectV2/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using InspectV2.ViewModels;
 
 namespace InspectV2.Controllers
 {
@@ -23,8 +24,18 @@ namespace InspectV2.Controllers
         public ActionResult Index()
         {
             var NewsCategories = _context.NewsCategories.ToList();
+            var news = _context.News.ToList();
 
-            return View(NewsCategories);
+            var viewModel = new IndexHomeViewModel();
+            viewModel.News = news;
+            viewModel.NewsCategories = NewsCategories;
+
+            return View(viewModel);
+        }
+        [Route("Home/newsdetails/{newsId}")]
+        public ActionResult NewsDetails(int newsId)
+        {
+            return Content("id = "+newsId);
         }
     }
 }
---
+++ InspectV2/InspectV2/ViewModels/IndexHomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using InspectV2.Models;
+
+namespace InspectV2.ViewModels
+{
+    public class IndexHomeViewModel
+    {
+        public List<NewsCategory> NewsCategories { get; set; }
+        public List<News> News { get; set; }
+    }
+}
