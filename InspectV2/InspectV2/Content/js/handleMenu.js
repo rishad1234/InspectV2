b11e948ede6c2584236b87a4a403cc773d17751a
Shafayet Ul Islam <35225819+shafayet98@users.noreply.github.com>
2020-09-23 22:57:29 +0600

    updated frontend files. added js

---
+++ InspectV2/InspectV2/Content/js/handleMenu.js
@@ -0,0 +1,44 @@
+﻿var getCurrentLoc = window.location.href;
+if (getCurrentLoc.includes("About")) {
+    $("#About").addClass("active");
+    $("#Event").removeClass("active");
+    $("#JobPost").removeClass("active");
+    $("#river").removeClass("active");
+    $("#Contact").removeClass("active");
+    $("#Home").removeClass("active");
+} else if (getCurrentLoc.includes("Event")) {
+    $("#About").removeClass("active");
+    $("#Event").addClass("active");
+    $("#JobPost").removeClass("active");
+    $("#river").removeClass("active");
+    $("#Contact").removeClass("active");
+    $("#Home").removeClass("active");
+} else if (getCurrentLoc.includes("JobPost")) {
+    $("#About").removeClass("active");
+    $("#Event").removeClass("active");
+    $("#JobPost").addClass("active");
+    $("#river").removeClass("active");
+    $("#Contact").removeClass("active");
+    $("#Home").removeClass("active");
+} else if (getCurrentLoc.includes("river")) {
+    $("#About").removeClass("active");
+    $("#Event").removeClass("active");
+    $("#JobPost").removeClass("active");
+    $("#river").addClass("active");
+    $("#Contact").removeClass("active");
+    $("#Home").removeClass("active");
+} else if (getCurrentLoc.includes("Contact")) {
+    $("#About").removeClass("active");
+    $("#Event").removeClass("active");
+    $("#JobPost").removeClass("active");
+    $("#river").removeClass("active");
+    $("#Contact").addClass("active");
+    $("#Home").removeClass("active");
+} else {
+    $("#About").removeClass("active");
+    $("#Event").removeClass("active");
+    $("#JobPost").removeClass("active");
+    $("#river").removeClass("active");
+    $("#Contact").removeClass("active");
+    $("#Home").addClass("active");
+}
